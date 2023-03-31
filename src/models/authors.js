80b261c9cd5d7932413f06c4f93ca78cc3f2b518
Tyler Jones <tylerjones@Tylers-Air.lan>
2023-03-30 18:58:08 -0700

    created schemas

--- src/models/authors.js
+++ src/models/authors.js
@@ -1,1 +1,24 @@
 // schema for authors
+
+// Dependencies
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const authorSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    startDate: {
+        type: Date,
+        required: true
+    },
+    Bio: {
+        type: String
+    }
+})
+
+// model and export
+const Author = mongoose.model('Author', authorSchema);
+
+module.exports = Author;
