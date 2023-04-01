de062678c80b014e9526eedae24106c4a717c700
Tyler Jones <tylerjones@Tylers-Air.lan>
2023-04-01 13:13:25 -0700

    worked on 404

--- src/App.js
+++ src/App.js
@@ -1,54 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import Slider from './Components/Slider';
+import Slider from './components/Slider';
+import Navbar from './components/Navbar';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import Default from './views/layouts/Default';
 
 
 const API= "http://localhost:3000"
 
 
 const App = () => {
-  const [authors, setAuthors] = useState([]);
-  const [selectedAuthor, setSelectedAuthor] = useState(null);
-
-  const fetchAuthors = async () => {
-    // Fetch authors from your API and update the state
-    // Replace with the correct API endpoint URL
-    const response = await fetch('/api/authors');
-    const data = await response.json();
-    setAuthors(data);
-  };
-
-  const handleFormSubmit = async (authorData) => {
-    // Create or update the author using your API
-    // Replace with the correct API endpoint URL
-    const response = await fetch('/api/authors', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(authorData),
-    });
-
-    if (response.ok) {
-      // Refresh the author list after a successful submission
-      fetchAuthors();
-    } else {
-      // Handle error
-      console.error('Error creating author:', await response.text());
-    }
-  };
-
-  const handleAuthorSelect = (author) => {
-    setSelectedAuthor(author);
-  };
-
-  useEffect(() => {
-    fetchAuthors();
-  }, []);
 
   return (
     <div>
+      <Navbar />
       <Slider />
       {/* Add other components or views here */}
     </div>
--- src/views/error404.jsx
+++ src/views/error404.jsx
@@ -1,1 +1,59 @@
 // error page
+import React from 'react';
+import { Box, Button, Container, Typography } from '@mui/material';
+import Grid from '@mui/material/Grid';
+
+export default function Error() {
+    
+    const handleClick = () => {
+        history.push('/');
+      };
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: '100vh'
+      }}
+    >
+      <Container maxWidth="md">
+        <Grid container spacing={2}>
+          <Grid xs={6}>
+            <Typography variant="h1">
+              404
+            </Typography>
+            <Typography variant="h6">
+              Sorry, this page was lost in transit..
+            </Typography>
+            <Button onClick={'handleClick'} variant="contained">Back Home</Button>
+          </Grid>
+          <Grid xs={6}>
+            <img
+              src="https://grownuptravelguide.com/wp-content/uploads/2020/04/AdobeStock_323158365.jpeg"
+              alt="Luggage not found."
+              width={500} height={250}
+            />
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
+}
+
+// const React = require('react');
+
+// const error404 = () => {
+//     return (
+//         <def>
+//             <main>
+//                 <h1>404: PAGE NOT FOUND</h1>
+//                 <img href="https://grownuptravelguide.com/wp-content/uploads/2020/04/AdobeStock_323158365.jpeg" alt="Luggage not found"></img>
+//                 <p>Sorry, this page got lost in transit!</p>
+//             </main>
+//         </def>
+//     )
+// }
+
+// module.export = error404
