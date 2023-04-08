df7a228a3724a928192bb85da7ec21dac564a297
Laila Rimando <112991420+LRimando@users.noreply.github.com>
2023-04-08 00:58:04 -0700

    Minor details for styling

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -24,18 +24,18 @@ const Error404 = () => {
         <Grid container spacing={2}>
           <Grid xs={6}>
             <Typography variant="h1" sx={{ fontFamily: 'system-ui', fontWeight: '900' }}>
-              404
+              404 Error 
             </Typography>
             <Typography variant="h6" sx={{ fontFamily: 'system-ui' }}>
-              Sorry, this page was lost in transit...
+              Oh no, this page was lost in transit...
             </Typography>
             <Button onClick={handleClick} variant="contained">Back Home</Button>
           </Grid>
           <Grid xs={6}>
             <img
               src="https://grownuptravelguide.com/wp-content/uploads/2020/04/AdobeStock_323158365.jpeg"
-              alt="Luggage not found."
-              width={500} height={250}
+              alt="Sorry, luggage not found."
+              width={600} height={300}
             />
           </Grid>
         </Grid>
